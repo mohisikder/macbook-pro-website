f6a9193cf7eaf13384f4bf0afd3fe20c27cd9e1d
Mohiuddin Sikder <mohi.info20@gmail.com>
2021-08-21 20:38:07 +0600

    updated

--- js/main.js
+++ js/main.js
@@ -51,25 +51,6 @@ function calculateUpdateTotal(){
 })
 }
 
-// mmmmii
-function validate(coupon) {
-   var myRe = "LUCKY100";
-   var coupon = myRe.trim();
-   var input = document.getElementById('in').value;
-   if(input.toUpperCase() == coupon.toUpperCase()) {
-       document.getElementById('message').innerHTML="Coupon applied!";
-       document.getElementById('err').innerHTML="";
-       return true;
-   } else {
-       document.getElementById('err').innerHTML="Invalid coupon";
-       document.getElementById('message').innerHTML="";
-       return false;
-   }
-}
-// mojia09
-
-
-
 // Memory 
 const minimumMemoryPrice = document.getElementById('default-cost')
 const extraMemoryPrice = document.getElementById('extra-cost')
